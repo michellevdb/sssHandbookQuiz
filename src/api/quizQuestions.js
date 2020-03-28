eea6549c0d722aa5b44166f28d689d0f0d028e59
Michelle Van Der Berg <michellecyou@gmail.com>
2020-03-28 11:55:50 -0700

    initial files

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,580 @@
+
+
+var quizQuestions = [
+  {
+      question: "What are the non-negotiables for the Safety Support Specialist position?",
+      answers: [
+          {
+              type: "false1",
+              content: "No Slack or work email on personal devices"
+          },
+          {
+              type: "false2",
+              content: "No alcohol consumption during work"
+          },
+          {
+              type: "false3",
+              content: "Be a team player"
+          },
+          {
+            type: "true",
+            content: "All answers are correct"
+          }
+      ]
+  },
+  {
+      question: "How will individual SSS success be measured as far as QA?",
+      answers: [
+          {
+              type: "Stark",
+              content: "Competency"
+          },
+          {
+              type: "Lannister",
+              content: "Process and protocols"
+          },
+          {
+              type: "Targaryen",
+              content: "Customer service and metrics"
+          },
+          {
+            type: "None",
+            content: "All answers are correct"
+          }
+      ]
+  },
+  {
+      question: "What is the code word if you need 5 minutes to collect yourself?",
+      answers: [
+          {
+              type: "Stark",
+              content: "Bubblegum"
+          },
+          {
+              type: "Lannister",
+              content: "Pineapple"
+          },
+          {
+              type: "Targaryen",
+              content: "Watermelon"
+          },
+          {
+            type: "None",
+            content: "Bacon"
+          }
+      ]
+  },
+  {
+      question: "What is our goal time frame for resolving every alert?",
+      answers: [
+          {
+              type: "Stark",
+              content: "5 Minutes"
+          },
+          {
+              type: "Lannister",
+              content: "20 Minutes"
+          },
+          {
+              type: "Targaryen",
+              content: "15 Minutes"
+          },
+          {
+            type: "None",
+            content: "30 Minutes"
+          }
+      ]
+  },
+  {
+      question: "When an alert is escalated, the OZ UI automatically sends emails to the Escalation List contacts?",
+      answers: [
+          {
+              type: "Stark",
+              content: "True"
+          },
+          {
+              type: "Lannister",
+              content: "False"
+          }
+      ]
+  },
+  {
+    question: "After deciding to de-escalate an alert, what step is taken next?",
+    answers: [
+        {
+            type: "Stark",
+            content: "Verify and re-categorize"
+        },
+        {
+            type: "Lannister",
+            content: "De-escalate and select 'complete'"
+        },
+        {
+            type: "Targaryen",
+            content: "Select 'away'"
+        },
+        {
+            type: "None",
+            content: "None of these are correct"
+        }
+    ]
+    },
+    {
+        question: "Which one of these Google search phrases would be considered Active Planning?",
+        answers: [
+            {
+                type: "Stark",
+                content: "I want to kill myself"
+            },
+            {
+                type: "Lannister",
+                content: "Painless way to kill yourself"
+            },
+            {
+                type: "Targaryen",
+                content: "Can you die from eating too many watermelon seeds"
+            },
+            {
+                type: "None",
+                content: "Noose"
+            }
+        ]
+    },
+    {
+        question: "What are the correct steps to take if a phone number doesn't work or you cannot leave a voicemail because the voicemail box is full?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Hang up, there is nothing you can do"
+            },
+            {
+                type: "Lannister",
+                content: "Make a note of it in Zendesk and Oz app, update the manager then move on to the next person on the escalation list"
+            },
+            {
+                type: "Targaryen",
+                content: "Call the last responder and let them know a number on the escalation list is incorrect"
+            },
+            {
+                type: "None",
+                content: "Email the person whose number is incorrect using the email provided on Oz. Ask for the correct number to update the list"
+            }
+        ]
+    },
+    {
+        question: "Is a second opinion required on all alerts before deciding to escalate or de-escalate?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Yes"
+            },
+            {
+                type: "Lannister",
+                content: "No"
+            },
+            
+        
+        ]
+    },
+    {
+        question: "If you come across an alert for a student or school that you have a personal connection to what should you do?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Move forward with workflow and handle alert"
+            },
+            {
+                type: "Lannister",
+                content: "Let your manager know of the personal connection and still handle the alert"
+            },
+            {
+                type: "Targaryen",
+                content: "Hit “away” and abstain from handling the alert"
+            },
+            {
+                type: "None",
+                content: "Close OZ app and clock out"
+            }
+        ]
+    },
+    {
+        question: "What is the TextExpander you would use when de-escalating an alert?",
+        answers: [
+            {
+                type: "Stark",
+                content: ";deescalate"
+            },
+            {
+                type: "Lannister",
+                content: ";ozdes"
+            },
+            {
+                type: "Targaryen",
+                content: ";ozman"
+            },
+            {
+                type: "None",
+                content: ";dealert"
+            }
+        ]
+    },
+    {
+        question: "TextExpander needs to be open in order for the keyboard shortcuts to work.",
+        answers: [
+            {
+                type: "Stark",
+                content: "True"
+            },
+            {
+                type: "Lannister",
+                content: "False"
+            }
+        ]
+    },
+    {
+        question: "What are the five levels of classification?",
+        answers: [
+            {
+                type: "Stark",
+                content: "AP, Ideation, Drama, Help & Support, Mental Illness"
+            },
+            {
+                type: "Lannister",
+                content: "AP, Ideation, Self-Harm, Help & Support, Research"
+            },
+            {
+                type: "Targaryen",
+                content: "AP, Ideation, Self-Harm, Cry for Help, Research"
+            },
+            {
+                type: "None",
+                content: "None of these answers are correct"
+            }
+        ]
+    },
+    {
+        question: "You can make a call directly from the Oz app.",
+        answers: [
+            {
+                type: "Stark",
+                content: "True"
+            },
+            {
+                type: "Lannister",
+                content: "False"
+            }
+        ]
+    },
+    {
+        question: "Which of the following is true about internal notes and public replies on Zendesk?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Public replies are used like a between two Safety Support Specialists whereas Internal notes are used for note taking"
+            },
+            {
+                type: "Lannister",
+                content: "Public replies are customer facing, whereas internal notes are internal and can only be seen by GoGuardian employees"
+            },
+            {
+                type: "Targaryen",
+                content: "Public replies are internal and can only be seen by GoGuardian employees and internal notes are customer facing and can also be seen by the requestor"
+            },
+            {
+                type: "None",
+                content: "Public replies and internal notes do the same thing"
+            }
+        ]
+    },
+    {
+        question: "What is the protocol if you get an upset or argumentative customer?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Apologize and call the manager to transfer the call"
+            },
+            {
+                type: "Lannister",
+                content: "Transfer customer to another Safety Support Specialist to handle"
+            },
+            {
+                type: "Targaryen",
+                content: "Apologize and continue workflow"
+            },
+            {
+                type: "None",
+                content: "Confirm details and phone number, Slack MOD, and continue workflow as usual"
+            }
+        ]
+    },
+    {
+        question: "Which of the following is false if an alert is in queue for longer than 30 minutes?",
+        answers: [
+            {
+                type: "Stark",
+                content: "All SSS will get a PagerDuty"
+            },
+            {
+                type: "Lannister",
+                content: "MOD and General Manager will get a PagerDuty"
+            },
+            {
+                type: "Targaryen",
+                content: "MOD will assess the cause of the issue"
+            },
+            {
+                type: "None",
+                content: "MOD and General Manager will review the causes of these instances in the weekly meeting"
+            }
+        ]
+    },
+    {
+        question: "What two platforms do SSS use to address alerts?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Slack and Oz UI"
+            },
+            {
+                type: "Lannister",
+                content: "Zendesk and Slack"
+            },
+            {
+                type: "Targaryen",
+                content: "Zendesk and Oz UI"
+            },
+            {
+                type: "None",
+                content: "Oz UI and Slack"
+            }
+        ]
+    },
+    {
+        question: "What phase/phases do SSS escalate to school admin?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Self-Harm"
+            },
+            {
+                type: "Lannister",
+                content: "Active Planning"
+            },
+            {
+                type: "Targaryen",
+                content: "Suicide Ideation"
+            },
+            {
+                type: "None",
+                content: "Active Planning and Suicide Ideation"
+            }
+        ]
+    },
+    {
+        question: "If you receive an alert for the same student a second time, you don't have to call the escalation list again.",
+        answers: [
+            {
+                type: "Stark",
+                content: "True"
+            },
+            {
+                type: "Lannister",
+                content: "False"
+            }
+        ]
+    },
+    {
+        question: "Why is it important for you to always lock your computer when stepping away from your desk?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Because someone might “Weeble Wobble” you"
+            },
+            {
+                type: "Lannister",
+                content: "Because no one at this company can be trusted"
+            },
+            {
+                type: "Targaryen",
+                content: "To protect student PII and sensitive information"
+            },
+            {
+                type: "None",
+                content: "It's not important to lock your computer when stepping away"
+            }
+        ]
+    },
+    {
+        question: "What Slack channel do you need to post the alert on to get a second opinion?",
+        answers: [
+            {
+                type: "Stark",
+                content: "#safety_support"
+            },
+            {
+                type: "Lannister",
+                content: "#ssschamps"
+            },
+            {
+                type: "Targaryen",
+                content: "#beacon"
+            },
+            {
+                type: "None",
+                content: "#s_cubed"
+            }
+        ]
+    },
+    {
+        question: "When an alert comes through that is a search (e.g., Google, Bing), SSS should focus on the DOM text.",
+        answers: [
+            {
+                type: "Stark",
+                content: "True"
+            },
+            {
+                type: "Lannister",
+                content: "False"
+            }
+        ]
+    },
+    {
+        question: "When typing notes in Zendesk, should they be internal facing or external facing?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Internal"
+            },
+            {
+                type: "Lannister",
+                content: "External"
+            }
+        ]
+    },
+    {
+        question: "If you have gone through the Escalation List twice and cannot reach anyone, what should you do?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Slack the General Manager about it"
+            },
+            {
+                type: "Lannister",
+                content: "Slack the team about it"
+            },
+            {
+                type: "Targaryen",
+                content: "Slack the General Manager and Team Manager about it"
+            },
+            {
+                type: "None",
+                content: "Do nothing other than 'complete' the escalation"
+            }
+        ]
+    },
+    {
+        question: "Is it okay to post PII in an open Slack channel or email?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Yes"
+            },
+            {
+                type: "Lannister",
+                content: "Sometimes depending on the situation"
+            },
+            {
+                type: "Targaryen",
+                content: "Never"
+            }
+        ]
+    },
+    {
+        question: "What do Safety Support Specialists not get QA’ed on?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Customer Service"
+            },
+            {
+                type: "Lannister",
+                content: "Process and Protocols"
+            },
+            {
+                type: "Targaryen",
+                content: "Timing"
+            },
+            {
+                type: "None",
+                content: "Competency"
+            }
+        ]
+    },
+    {
+        question: "What should you do if any media or press has any inquiries about GoGuardian?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Report the incident to IT"
+            },
+            {
+                type: "Lannister",
+                content: "Decline any requests and reach out to GM and MOD"
+            },
+            {
+                type: "Targaryen",
+                content: "Respond the request but be careful not to provide PII"
+            },
+            {
+                type: "None",
+                content: "Ignore or delete any requests"
+            }
+        ]
+    },
+    {
+        question: "What phase would the Google search “suicide” be caterorized under?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Help and Resources"
+            },
+            {
+                type: "Lannister",
+                content: "Active Planning"
+            },
+            {
+                type: "Targaryen",
+                content: "Research"
+            },
+            {
+                type: "None",
+                content: "Self-Harm"
+            }
+        ]
+    },
+    {
+        question: "When an alert is de-escalated, what are the next steps that should be taken?",
+        answers: [
+            {
+                type: "Stark",
+                content: "Fill out Oz notes and 'Complete'"
+            },
+            {
+                type: "Lannister",
+                content: "De-escalated alerts require no additional steps"
+            },
+            {
+                type: "Targaryen",
+                content: "Fill out Oz notes and send emails from Zendesk"
+            },
+            {
+                type: "None",
+                content: "Fill out Oz notes then add the alert to the 'De-escalated Alerts' form"
+            }
+        ]
+    },
+
+];
+ 
+export default quizQuestions;
+
